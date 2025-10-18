bded02aa866f0740263cd67f80effe73d63d221b
muszka <zuzanna.fojtar@gmail.com>
2025-10-18 16:57:22 +0200

    Dwa pierwsze zadania.

---
+++ Lista 1/obliczanie wart w(x).py
@@ -0,0 +1,37 @@
+coeff = [4, 6, 8]
+# 1. O(n^2)
+def wiel_wart(x, coeff):
+    """
+    This is a function calculating the value of a polynomial for given value of x and list of coefficients.
+    :param x: int variable
+    :param coeff: list of numbers
+    :return: int result p(a) where 'a' is a given x
+    """
+    res = coeff[0]
+    for i in range(1, len(coeff)):
+        res += coeff[i] * x ** i
+    return res
+
+print(wiel_wart(2, coeff))
+
+# 2. O(n logn)
+def wiel_wart_rek(x, coeff):
+    """
+    *Same* but using recursion to calculate the values of x to the power of n.
+    :param x:
+    :param coeff:
+    :return:
+    """
+    res = coeff[0]
+    for i in range(1,len(coeff)):
+        if i // 2 == 0:
+            half = x ** (1 // 2)
+            res += coeff[i] * half * half
+        else:
+            res += coeff[i] * half * half * x
+    return res
+
+print(wiel_wart(2, coeff))
+
+# 3. Horner
+'''Aby obliczyć wartość wielomianu w postaci Hornera, należy wykonać n podstawień danej wartości w zamian za x oraz n - 1 mnożeń, co daje 2n - 1 operacji, gdzie stopień wielomianu jest większy lub równy n.'''
---
+++ Lista 1/porownanie_O.py
@@ -0,0 +1,74 @@
+import matplotlib.pyplot as plt
+import random
+import time
+
+
+def example1(S):
+    """Return the sum of the elements in sequence S."""
+    n = len(S)
+    total = 0
+    for j in range(n):
+        total += S[j]
+        return total
+
+def example2(S):
+    """Return the sum of the elements with even index in sequence S."""
+    n = len(S)
+    total = 0
+    for j in range(0, n, 2):
+        total += S[j]
+    return total
+
+def example3(S):
+    """Return the sum of the prex sums of sequence S."""
+    n = len(S)
+    total = 0
+    for j in range(n):
+        for k in range(1+j):
+            total += S[k]
+    return total
+
+def example4(A, B): # assume that A and B have equal length
+    """Return the number of elements in B equal to the sum of prex sums in A."""
+    n = len(A)
+    count = 0
+    for i in range(n):
+        total = 0
+    for j in range(n):
+        for k in range(1 + j):
+            total += A[k]
+    if B[i] == total:
+        count += 1
+    return count
+
+num_of_elem = [10**i for i in range(2,5)]
+li_of_li = []
+for n in num_of_elem:
+    li = random.sample(range(1, 10001), n)
+    li_of_li.append(li)
+
+functions = [example1, example2, example3, example4]
+for f in functions:
+    print(f"Testing function {f.__name__}.")
+    li_of_time = []
+    for j in li_of_li:
+        start = time.time()
+        if f == example4:
+            example4(j, j)
+        else:
+            f(j)
+        end = time.time()
+        li_of_time.append(end - start)
+
+    plt.plot(num_of_elem, li_of_time, label=f.__name__)
+
+    plt.xlabel("Number of Elements (n)")
+    plt.ylabel("Execution Time (seconds)")
+    plt.title("Performance Comparison of Functions")
+    plt.legend()
+    plt.show()
+
+
+
+
+
