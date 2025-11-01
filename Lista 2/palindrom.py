dfc82596be7dd663690cc278a10054517bce3154
muszka <zuzanna.fojtar@gmail.com>
2025-11-01 20:13:59 +0100

    Działa, zostało przetestować na nie-palindromie.

---
+++ Lista 2/palindrom.py
@@ -0,0 +1,14 @@
+def is_palindrom(sl):
+    if type(sl) != str:
+        print("To nie jest słowo!")
+    else:
+        sl = sl.upper()
+        for i in range(len(sl)):
+            if sl[i] != sl[len(sl) - i - 1]:                        # -1, żeby indeksy się zgadzały
+                return False
+            return True
+
+if is_palindrom('kajAk'):
+    print('Jest palindromem')
+else:
+    print('Nie jest palindormem')
