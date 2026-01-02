7ed045f25ad7a633bb29719e1f684ce313f265e2
muszka <zuzanna.fojtar@gmail.com>
2026-01-02 14:31:59 +0100

    EXAMFUN, za pomocą klasy BinaryTree

---
+++ Lista 4/binary_tree_T.py
@@ -0,0 +1,13 @@
+from binary_tree import BinaryTree
+
+tT = BinaryTree()
+
+tT.set_root("E")
+tT.set_left(0, "X")
+tT.set_right(0, "N")
+tT.set_left(1, "A")
+tT.set_right(1, "U")
+tT.set_left(3,"M")
+tT.set_right(3, "F")
+
+tT.draw()
